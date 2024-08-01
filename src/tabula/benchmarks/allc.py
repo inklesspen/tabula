6255c899da32783efde9c71e3b4a0ded6e68321f
Rose Davidson <rose@metaclassical.com>
2024-08-01 12:36:13 +0100

    Benchmarks for markdown parsing
    On dev machine:
    python -m timeit -s "from tabula.benchmarks.allc import undertest" "undertest()"
    500 loops, best of 5: 803 usec per loop
    
    python -m timeit -s "from tabula.benchmarks.legacy import undertest" "undertest()"
    20 loops, best of 5: 18.6 msec per loop

---
+++ src/tabula/benchmarks/allc.py
@@ -0,0 +1,22 @@
+from ..rendering._cairopango import ffi, lib  # type: ignore
+
+
+def fully_free_gstring(gstring):
+    lib.g_string_free(gstring, True)
+
+
+DRACULA = """_3 May. Bistritz._⁠—Left Munich at 8:35 p.m., on 1st May, arriving at Vienna early next morning; should have arrived at 6:46, but train was an hour late. Buda-Pesth seems a wonderful place, from the glimpse which I got of it from the train and the little I could walk through the streets. I feared to go very far from the station, as we had arrived late and would start as near the correct time as possible. The impression I had was that we were leaving the West and entering the East; the most western of splendid bridges over the Danube, which is here of noble width and depth, took us among the traditions of Turkish rule."""  # noqa: E501
+
+fontmap = lib.pango_cairo_font_map_get_default()
+context = ffi.gc(lib.pango_font_map_create_context(fontmap), lib.g_object_unref)
+layout = ffi.gc(lib.pango_layout_new(context), lib.g_object_unref)
+
+
+def undertest():
+    gstr = ffi.gc(lib.g_string_sized_new(len(DRACULA)), fully_free_gstring)
+    mstate = ffi.new("MarkdownState*")
+    for letter in DRACULA:
+        lib.g_string_append_unichar(gstr, ord(letter))
+        lib.markdown_attrs(mstate, gstr)
+        lib.pango_layout_set_text(layout, gstr.str, gstr.len)
+        lib.pango_layout_set_attributes(layout, mstate.attr_list)
